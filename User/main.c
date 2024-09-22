8749a17418cd52f9bd909ce56c7493cc196f8550
yy <2690911036@qq.com>
2024-09-22 17:50:21 +0800

    first commit

---
+++ User/main.c
@@ -0,0 +1,71 @@
+#include "stm32f10x.h"                  // Device header
+#include "SysTick.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "LED.h"
+#include "Key.h"
+
+//任务优先级
+#define START_TASK_PRIO		1
+//任务堆栈大小	
+#define START_STK_SIZE 		128  
+//任务句柄
+TaskHandle_t StartTask_Handler;
+//任务函数
+void start_task(void *pvParameters);
+
+//任务优先级
+#define LED1_TASK_PRIO		2
+//任务堆栈大小	
+#define LED1_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED1Task_Handler;
+//任务函数
+void led1_task(void *pvParameters);
+
+int main(void)
+{
+	SysTick_Init(72);
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4
+	/*模块初始化*/
+	LED_Init();		//LED初始化
+
+		//创建开始任务
+    xTaskCreate((TaskFunction_t )start_task,            //任务函数
+                (const char*    )"start_task",          //任务名称
+                (uint16_t       )START_STK_SIZE,        //任务堆栈大小
+                (void*          )NULL,                  //传递给任务函数的参数
+                (UBaseType_t    )START_TASK_PRIO,       //任务优先级
+                (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
+    vTaskStartScheduler(); 
+}
+
+
+//开始任务任务函数
+void start_task(void *pvParameters)
+{
+    taskENTER_CRITICAL();           //进入临界区
+      
+    //创建LED1任务
+    xTaskCreate((TaskFunction_t )led1_task,     
+                (const char*    )"led1_task",   
+                (uint16_t       )LED1_STK_SIZE, 
+                (void*          )NULL,
+                (UBaseType_t    )LED1_TASK_PRIO,
+                (TaskHandle_t*  )&LED1Task_Handler); 
+				
+    vTaskDelete(StartTask_Handler); //删除开始任务
+    taskEXIT_CRITICAL();            //退出临界区
+} 
+
+//LED1任务函数
+void led1_task(void *pvParameters)
+{
+    while(1)
+    {
+        LED1_ON();
+        vTaskDelay(200);
+        LED1_OFF();
+        vTaskDelay(800);
+    }
+}
